be3c72edd37ec20c3f9a90c0a5507c2f48c0a0f0
Nick Pitrak <pitrak1@gmail.com>
2021-04-15 21:40:02 -0500

    add turn indicator

---
+++ TurnIndicator.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var __player_name
+
+func _ready():
+	$Label.text = ""
+
+func set_player(player_name):
+	self.__player_name = player_name
+	
+func set_turn(player_name):
+	if (self.__player_name == player_name):
+		$Label.text = "Your turn"
+	else:
+		$Label.text = player_name + "'s turn"
