bcaba02ba09950c18191e083c7d8a9c82e134ec5
Nick Pitrak <pitrak1@gmail.com>
2021-04-16 21:26:42 -0500

    add tile stack

---
+++ TileStack.gd
@@ -0,0 +1,28 @@
+extends Node
+
+class Tile:
+	func _init(shape, color):
+		self.shape = shape
+		self.color = color
+		
+	var shape
+	var color
+	
+var constants_script = preload("res://Constants.gd")
+var constants
+
+var stack = []
+
+func _init(shuffle=true):
+	self.constants = constants_script.new()
+	for color in self.constants.COLORS:
+		for shape in self.constants.SHAPES:
+			self.stack.append(Tile.new(shape, color))
+			self.stack.append(Tile.new(shape, color))
+			self.stack.append(Tile.new(shape, color))
+	
+	if shuffle:
+		self.stack.shuffle()
+	
+func draw():
+	return self.stack.pop_back()
