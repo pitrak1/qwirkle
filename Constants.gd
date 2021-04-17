226b2d5ba2b73e7625302a073ff2856936f36183
Nick Pitrak <pitrak1@gmail.com>
2021-04-16 20:15:47 -0500

    add grid position and shape/color drawing

---
+++ Constants.gd
@@ -0,0 +1,14 @@
+extends Node
+
+enum SHAPES {SUN, STAR, CLOVER, DIAMOND, SQUARE, CIRCLE}
+enum COLORS {RED, BLUE, GREEN, PURPLE, YELLOW, ORANGE}
+enum DIRECTIONS {UP, RIGHT, DOWN, LEFT}
+
+var COLOR_OBJECTS = [
+	Color(1, 0, 0, 1),
+	Color(0, 0, 1, 1),
+	Color(0, 1, 0, 1),
+	Color(1, 0, 1, 1),
+	Color(1, 1, 0, 1),
+	Color(1, 0.5, 0, 1)
+]
--- GridPosition.gd
+++ GridPosition.gd
@@ -1,7 +1,49 @@
 extends TextureRect
 
-var highlight_texture = preload("res://assets/GridPositionHighlight.png")
-var error_texture = preload("res://assets/GridPositionError.png")
+var sun_texture = preload("res://assets/Sun.png")
+var star_texture = preload("res://assets/Star.png")
+var clover_texture = preload("res://assets/Clover.png")
+var diamond_texture = preload("res://assets/Diamond.png")
+var square_texture = preload("res://assets/Square.png")
+var circle_texture = preload("res://assets/Circle.png")
+
+var constants_script = preload("res://Constants.gd")
+var constants
 
 func _ready():
-	self.texture = highlight_texture
+	constants = constants_script.new()
+
+func set_highlight():
+	$Error.hide()
+	$Highlight.show()
+	
+func set_error():
+	$Highlight.hide()
+	$Error.show()
+		
+func clear_overlay():
+	$Highlight.hide()
+	$Error.hide()
+	
+func set_shape(shape, color):
+	$Shape.show()
+	
+	match shape:
+		constants.SHAPES.SUN:
+			$Shape.texture = sun_texture
+		constants.SHAPES.STAR:
+			$Shape.texture = star_texture
+		constants.SHAPES.CLOVER:
+			$Shape.texture = clover_texture
+		constants.SHAPES.DIAMOND:
+			$Shape.texture = diamond_texture
+		constants.SHAPES.SQUARE:
+			$Shape.texture = square_texture
+		constants.SHAPES.CIRCLE:
+			$Shape.texture = circle_texture
+			
+	$Shape.modulate = constants.COLOR_OBJECTS[color]
+	
+func clear_shape():
+	$Shape.hide()
+	
