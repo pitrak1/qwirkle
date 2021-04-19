8a26f88f267a43d51fa36aa95874c9203c925755
Nick Pitrak <pitrak1@gmail.com>
2021-04-18 20:39:51 -0500

    correct grid positioning

--- Main.gd
+++ Main.gd
@@ -1,6 +1,11 @@
 extends Control
 
+var constants_script = preload("res://src/Constants.gd")
+var constants
+
 func _ready():
+	constants = constants_script.new()
 	$TurnIndicator.set_turn("Player 1")
+	$Board.initialize(constants.BOARD_SIZE)
 
 
